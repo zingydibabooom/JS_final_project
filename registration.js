5c8dd4d05920d029725177e733af7f4a8707318a
zing <zing@zings-MacBook-Pro.local>
2023-03-21 01:02:21 +0000

    random

--- registration.js
+++ registration.js
@@ -1,43 +1,36 @@
-$(document).ready(function () {
+const express = require("express");
 
-});
-
-function collect(){
+$("#submit")[0].addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log();
+})
+function collect() {
     console.log("run");
-    const userName = document.querySelector('#username');
-    const passWord = document.querySelector('#password');
-    const email = document.querySelector('#email');
+    const userName = document.querySelector('#username').value;
+    const passWord = document.querySelector('#password').value;
+    const email = document.querySelector('#email').value;
     console.log(userName, passWord, email);
-};
-// const collectRegiData= document.querySelector('#submitBtn');
 
+    require("dotenv").config();
+    const DBCONFIG = {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD
+    };
 
+    const connection = mysql.createConnection(DBCONFIG);
+    connection.connect(function (err) {
+        const query = `INSERT INTO user_credential(\
+        username, password, email\
+    ) VALUES (\
+        ${userName}, ${passWord}, ${email}\
+    );`
 
+        connection.query(query, function (err, result, fields) {
+            console.log(result);
+        });
+        connection.end();
+    });
+}
 
-
-// require("dotenv").config();
-// const DBCONFIG = {
-// host: process.env.DB_HOST, 
-// database: process.env.DB_NAME,
-// user: process.env.DB_USERNAME,
-// password: process.env.DB_PASSWORD
-// };
-
-// const connection = mysql.createConnection( DBCONFIG ); 
-// connection.connect(function(err) {
-//     const query = "INSERT INTO user_credential(\
-//         username, password, email\
-//     ) VALUES (\
-//         userName, passWord, email\
-//     );"
-
-//     connection.query(query, function (err, result, fields) {
-//         console.log(result);
-//   });
-//  });
-
-//  connection.end();
-
-// form.addEventListener('button', e=> {
-//     e.preventDefault();
-// });
