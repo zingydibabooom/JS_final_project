d438cbc798c4bd91938a803d8e4836e208aed6e8
zing <zing@zings-MacBook-Pro.local>
2023-03-22 18:12:11 +0000

    sql connection

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var mysql = require("mysql");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -16,22 +18,76 @@ app.get("/registration", (req, res) => {
 })
 
 
-app.post("/addUserCredentials", urlencodedParser, validationObject, async(req, res) => {
-    let email = req.body.email;
+const { check, validationResult } = require("express-validator");
+//define the validation rules
+let validationRule = [
+    //check the fields() of the body 
+    check("username").exists().trim().escape(),
+    // check(password).mixedCase().numbers().letters().min(8),
+    check("email").trim().isEmail().escape(),
+];
+app.post("/addUserCredentials", urlencodedParser, validationRule, async (req, res) => {
+
     let username = req.body.username;
     let password = req.body.password;
-    console.log(email);
-    const { check, validationResult } = require("express-validator");
-    let validationObject = [
-        check(username).exists().trim().escape(),
-        // check(password).mixedCase().numbers().letters().min(8),
-        check(email).trim().isEmail().escape(),
-    ];
+    let email = req.body.email;
+    // console.log(req.body.username);
+    // console.log(req.body.email);
+
+    //get the data from the server
+    const dotenv = require("dotenv");
+    dotenv.config();
+    const DBCONFIG = {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD
+    };
+
+    const connection = mysql.createConnection(DBCONFIG);
+    connection.connect(function (err) {
+        const query = `INSERT INTO user_credential(\
+        username, password, email\
+    ) VALUES (\
+        ${username}, ${password}, ${email}\
+    );`
+        console.log(username);
+
+        connection.query(query, function (err, result, fields) {
+            console.log(result);
+
+        });
+        connection.end();
+    });
+
+    //connection builder
+    // const QueryBuilder = require('node-querybuilder');
+    // const pool = new QueryBuilder(DBCONFIG, 'mysql', 'pool');
+
+    // pool.get_connection(qb => {
+    //     qb.select('*')
+    //     .get(user_credential, (err, results) => {
+    //         qb.disconnect();
+    //         console.log("Results:", results);
+    //     }
+    //     );
+    // });
+
+    //Error messages
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-     //maybe?
-        console.log("error");
+        // console.log("run");
+        const errorArray = errors.array();
+
+        for (let i = 0; i < errorArray.length; i++) {
+            console.log(errorArray[i]);
+            const currentError = errorArray[i].param;
+            if (currentError == 'email') {
+                console.log("Please insert proper email address.")
+            }
+        }
     }
+
 });
 
 
@@ -56,28 +112,5 @@ app.listen(PORT, () => {
 //     const email = document.querySelector('#email').value;
 //     console.log(userName, passWord, email);
 
-//     const dotenv = require("dotenv");
-//     dotenv.config();
-//     const DBCONFIG = {
-//         host: process.env.DB_HOST,
-//         database: process.env.DB_NAME,
-//         user: process.env.DB_USERNAME,
-//         password: process.env.DB_PASSWORD
-//     };
 
-//     const connection = mysql.createConnection(DBCONFIG);
-//     connection.connect(function (err) {
-//         const query = `INSERT INTO user_credential(\
-//         username, password, email\
-//     ) VALUES (\
-//         ${userName}, ${passWord}, ${email}\
-//     );`
-
-
-//         connection.query(query, function (err, result, fields) {
-//             console.log(result);
-//         });
-//         connection.end();
-//     });
-// }
 
